022b44cd0710da8a5c78f5d6b6b70bd08b22970d
onggiaolang70 <vanhahoahoc789@gmail.com>
2026-02-11 23:05:49 +0700

    Update supabaseConfig.ts

--- config/supabaseConfig.ts
+++ config/supabaseConfig.ts
@@ -4,7 +4,7 @@
  */
 
 export const supabaseConfig = {
-  url: 'https://jaeafujranojcpixqftg.supabase.co',
+  url: 'https://https://nnhtkdqvuozqhjtutxls.supabase.co',
   anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5uaHRrZHF2dW96cWhqdHV0eGxzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA4MTUyNTYsImV4cCI6MjA4NjM5MTI1Nn0.OMG6ph94T_woTm0hEF9iaH8_o-Ndiz3Z2zc31Wppv6w'
 };
 
